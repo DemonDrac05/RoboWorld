c758b5e43e6e0511445a398eff4bf29ce5a5edd9
DemonDrac05 <phantien.thang@vvk.lt>
2024-11-30 13:05:55 +0300

    Fix SO not saving

--- Assets/Scripts/Stat/Base/BaseStatSO.cs
+++ Assets/Scripts/Stat/Base/BaseStatSO.cs
@@ -18,30 +18,6 @@ namespace Game.Stats
         public float rangeWeaponSpeed;
         public float meleeWeaponSpeed;
     }
-
-    [CreateAssetMenu(menuName = "Stats/Player Stats")]
-    public class PlayerStatSO : BaseStatSO
-    {
-        [Header("=== Basic Addition =========")]
-        public float maxStamina;
-
-        [Header("=== Weapon Damage Addition ==========")]
-        public float missileDamage;
-        public float missileAOEDamage;
-
-        [Header("=== Weapon Speed Addition ==========")]
-        public float missileLaunchingTime;
-        public float missileLaunchingCooldown;
-
-        [Header("=== Unique Stats ==========")]
-        [Range(0f, 1f)] public float piercingPercentage;
-
-        [Range(0f, 1f)] public float shieldRecPercentage;
-        [Range(0f, 1f)] public float shieldRecDuration;
-
-        [Range(0f, 1f)] public float staminaRecPercentage;
-        [Range(0f, 1f)] public float staminaRecDuration;
-    }
 }
 
 
---
+++ Assets/Scripts/Stat/Base/PlayerStatSO.cs
@@ -0,0 +1,26 @@
+using Game.Stats;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Stats/Player Stats")]
+public class PlayerStatSO : BaseStatSO
+{
+    [Header("=== Basic Addition =========")]
+    public float maxStamina;
+
+    [Header("=== Weapon Damage Addition ==========")]
+    public float missileDamage;
+    public float missileAOEDamage;
+
+    [Header("=== Weapon Speed Addition ==========")]
+    public float missileLaunchingTime;
+    public float missileLaunchingCooldown;
+
+    [Header("=== Unique Stats ==========")]
+    [Range(0f, 1f)] public float piercingPercentage;
+
+    [Range(0f, 1f)] public float shieldRecPercentage;
+    [Range(0f, 1f)] public float shieldRecDuration;
+
+    [Range(0f, 1f)] public float staminaRecPercentage;
+    [Range(0f, 1f)] public float staminaRecDuration;
+}
